2b5e0584056b49d1e49832cdbc2ae4f61cfacf11
ihalseide <imitatio-ignotum@protonmail.com>
2021-02-21 20:36:25 -0700

    Setup some test files and a script

---
+++ tests/nbttrip.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    import nbt, sys, gzip
+
+    if len(sys.argv) != 3:
+        print('Usage:', sys.argv[0], '[input filename]' '[output filename]')
+        sys.exit(-1) 
+
+    filename_in = sys.argv[1]
+    filename_out = sys.argv[2]
+
+    data = nbt.NBTFile(filename_in)
+
+    # .bin file is uncompressed
+    with open('%s.bin' % filename_out, 'wb') as outfile:
+        nbt.write_named_tag(outfile, data.compound)
+
+    # nbt file is compressed with gzip
+    with gzip.open(filename_out, 'wb') as outfile:
+        nbt.write_named_tag(outfile, data.compound)
