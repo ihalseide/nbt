45760b330e034b26c782887c5632846096ac695f
Izak Halseide <ihals@colostate.edu>
2024-04-20 16:07:44 -0600

    make nbt printer work better with lists

--- nbt/print.py
+++ nbt/print.py
@@ -1,51 +1,86 @@
 from .nbt import *
 
-def print_tag (tag: Tag, indent=0, indent_str='    '): 
+def print_tag(tag: Tag, indent=0, indent_str='    ', within_list=False): 
     '''Print out an NBT tag and all of its contents, except for TAG_BYTE_ARRAY.'''
     tag_type = tag.kind 
     namestr = tag.get_name_str()
-    namestr = f"\"{namestr}\""
+    if namestr:
+        namestr = f"\"{namestr}\""
 
+    # Indent the line
     print(indent_str * indent, end='') 
 
     if TagKind.END == tag_type:
         print('(end)')
         print('    ' * (indent - 1), '}', sep='')
     elif TagKind.BYTE == tag_type:
-        print(f"byte {namestr} = {tag.val_int}")
+        print(end="byte")
+        if not within_list: print(end=f" {namestr} =")
+        print(f" {tag.val_int}")
     elif TagKind.SHORT == tag_type:
-        print('short %s = %d' % (namestr, tag.val_int))
+        print(end="short")
+        if not within_list: print(end=f" {namestr} =")
+        print(f" {tag.val_int}")
     elif TagKind.INT == tag_type:
-        print('int %s = %d' % (namestr, tag.val_int))
+        print(end="int")
+        if not within_list: print(end=f" {namestr} =")
+        print(f" {tag.val_int}")
     elif TagKind.LONG == tag_type:
-        print('long %s = %d' % (namestr, tag.val_int))
+        print(end="long")
+        if not within_list: print(end=f" {namestr} =")
+        print(f" {tag.val_int}")
     elif TagKind.FLOAT == tag_type:
-        print('float %s = %f' % (namestr, tag.val_float))
+        print(end="float")
+        if not within_list: print(end=f" {namestr} =")
+        print(f" {tag.val_float}")
     elif TagKind.DOUBLE == tag_type:
-        print('double %s = %f' % (namestr, tag.val_float))
+        print(end="double")
+        if not within_list: print(end=f" {namestr} =")
+        print(f" {tag.val_float}")
     elif TagKind.STRING == tag_type:
-        print('string [%d] %s = "%s"' % (tag.get_length(), namestr, tag.val_str))
+        print(end=f"string [{tag.get_length()}]")
+        if not within_list: print(end=f" {namestr} =")
+        print(f" \"{tag.val_str}\"")
     elif TagKind.LIST == tag_type:
-        print('list %s [%d] %s = {' % (TagKind.to_str(tag.get_item_kind()), tag.get_length(), namestr))
+        element_kind = TagKind.to_str(tag.get_item_kind())[4:]
+        element_count = tag.get_length()
+        print(end=f"list<{element_kind}> [{element_count}]")
+        print(f" {namestr} = ""{")
         for sub_tag in tag.val_list:
-            print_tag(sub_tag, indent + 1)
-        print('    ' * indent, '}', sep='')
+            print_tag(sub_tag, indent + 1, within_list=True)
+        print(indent_str * indent, '}', sep='')
     elif TagKind.BYTE_ARRAY == tag_type:
-        # Don't print byte arrays because they tend to contain lots of binary data
-        print('bytearray [%d] %s = {' % (tag.get_length(), namestr))
-        print(', '.join(str(t) for t in tag.val_bytes))
+        length = tag.get_length()
+        row_width = 20
+        print(end=f"byte_array [{length}]")
+        if not within_list: print(end=f" {namestr} =")
+        print(" {")
+        print(indent_str * (indent + 1), end='') 
+        for i, b in enumerate(tag.val_bytes):
+            print(str(b), end=' ')
+            if (i > 0) and (i % row_width == 0) and (i != len(tag.val_bytes) - 1):
+                print()
+                print(indent_str * (indent + 1), end='') 
+        print()
         print(indent_str * indent, '}', sep='')
     elif TagKind.COMPOUND == tag_type:
-        print('compound (%d) %s = {' % (len(tag.val_list) - 1, namestr))
+        print(end="compound")
+        if not within_list: print(end=f" {namestr} =")
+        print(" {")                          
         for sub_tag in tag.val_list:
             print_tag(sub_tag, indent + 1)
-        print(indent_str * indent, '}', sep='')
+        # Ending gets printed by TAG_END
+        #print(indent_str * indent, '}', sep='')
     elif TagKind.INT_ARRAY == tag_type:
-        print('int array [%d] %s = {' % (tag.get_length(), namestr))
+        print(end=f"int_array [{tag.get_length()}]")
+        if not within_list: print(end=f" {namestr} =")
+        print(" {")  
         print(',\n'.join(str(t.val_int) for t in tag.val_list))
         print(indent_str * indent, '}', sep='')
     elif TagKind.LONG_ARRAY == tag_type:
-        print('long array [%d] %s = {' % (tag.get_length(), namestr))
+        print(end=f"long_array [{tag.get_length()}]")
+        if not within_list: print(end=f" {namestr} =")
+        print(" {") 
         print(',\n'.join(str(t.val_int) for t in tag.val_list))
         print(indent_str * indent, '}', sep='')
     else:
