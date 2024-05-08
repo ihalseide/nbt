a349d1f2af940c8f94caca086026fcb777000cf9
Izak Halseide <ihals@colostate.edu>
2024-05-08 08:06:34 -0600

    change test script to have some options

--- tests/crw_test.py
+++ tests/crw_test.py
@@ -4,26 +4,33 @@ Create-read-write test for the nbt module.
 
 import nbt
 import gzip
-from nbt import NamedTag, TagCompound, TagString
-from nbt import print_tag
+from nbt import NamedTag, TagCompound, TagString, TagShort
+from nbt.printing import print_tag
+
+do_read = True
+do_write = True
 
 print("Creating...")
 # <EditMe>
 # test whatever for the value of x
-x = NamedTag("root", TagCompound([
-    
-]))
+x = NamedTag("root", TagCompound({
+    'x': TagString('X value'),
+    'short value lol': TagShort(555555)
+}))
 # </EditMe>
+
 print("x =", x)
 print_tag(x)
 
-print("Writing...")
-with open("small_test.nbt", "wb") as f1:
-    x.write_to_file(f1)
-del x
+if do_write:
+    print("Writing...")
+    with open("small_test.nbt", "wb") as f1:
+        x.write_to_file(f1)
 
-print("Reading...")
-with open("small_test.nbt", "rb") as f2:
-    y = NamedTag.read_from_file(f2)
-print("y =", y)
-print_tag(y)
+if do_read:
+    print("Reading...")
+    with open("small_test.nbt", "rb") as f2:
+        y = NamedTag.read_from_file(f2)
+
+    print("y =", y)
+    print_tag(y)
